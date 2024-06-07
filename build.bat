64170a7f5ee6609a465bd2d75fbcec1f407c2ed6
Alexander-Tengborg <Landervich@Gmail.com>
2024-06-07 22:16:23 +0200

    Delete build.bat

--- build.bat
+++
@@ -1,9 +0,0 @@
-g++ -c .\src\main.cpp -o .\build\main.o -I W:\SFML\include
-g++ -c .\src\Game.cpp -o .\build\Game.o -I W:\SFML\include
-g++ -c .\src\GameSounds.cpp -o .\build\GameSounds.o -I W:\SFML\include
-g++ -c .\src\TetrisShape.cpp -o .\build\TetrisShape.o -I W:\SFML\include
-g++ -c .\src\Ui.cpp -o .\build\Ui.o -I W:\SFML\include
-
-::g++ .\main.o .\TetrisShape.o -o main.exe -L W:\SFML\lib -lsfml-system -lmingw32 -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-main -mwindows -lopengl32 -lwinmm -lgdi32 -DSFML_STATIC
-g++ .\build\main.o .\build\Game.o .\build\GameSounds.o .\build\TetrisShape.o .\build\Ui.o -o .\build\main.exe -L W:\SFML\lib -lsfml-system -lmingw32 -lsfml-graphics -lsfml-window -lsfml-audio -lopengl32 -lwinmm -lgdi32 -DSFML_STATIC
-::Second command removes -lsfml-main -mwindows which allows you to use the terminal
